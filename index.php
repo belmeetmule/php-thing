ddd43b28a18526d849e21243c69f86e059cb4d9f
mule <belmeetmule@gmail.com>
2023-09-23 14:58:05 +0300

    Pizza order calculator

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP tutorial</title>
+</head>
+<body>
+<div>
+    <form action="index.php" method="post">
+        <label for="quantity">Quantity</label> <br>
+        <input type="text" name="quantity"> <br>
+        <input type="submit" value="Order">
+    </form>
+</div>
+    
+</body>
+</html>
+
+<?php
+
+$item= "pizza";
+$price= 10;
+
+
+if(empty($_POST["quantity"]))
+    echo "Please enter a quantity"; 
+else
+{
+    $quantity= (int)$_POST["quantity"];
+    $total= $price * $quantity;
+    echo "You ordered " . $quantity . " " . $item . "s <br>";
+    echo "Total cost is " . " <strong>$ {$total} </strong> ". " <br>";
+    echo "Thank you for ordering from us";
+}
+     
+?>
